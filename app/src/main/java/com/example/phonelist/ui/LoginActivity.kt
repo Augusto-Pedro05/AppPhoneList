23840f42db49a54da06c7d8261dad326209c9ba9
Augusto-Pedro05 <pedroaugustopr2005@gmail.com>
2025-01-31 15:49:22 -0300

    Update fun login, add sharedPreferences

--- app/src/main/java/com/example/phonelist/ui/LoginActivity.kt
+++ app/src/main/java/com/example/phonelist/ui/LoginActivity.kt
@@ -1,17 +1,22 @@
 package com.example.phonelist.ui
 
+import android.content.Context
 import android.content.Intent
+import android.content.SharedPreferences
 import android.os.Bundle
+import android.widget.Toast
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 import com.example.phonelist.R
+import com.example.phonelist.database.DBHelper
 import com.example.phonelist.databinding.ActivityLoginBinding
 
 class LoginActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivityLoginBinding
+    private lateinit var sharedPreferences: SharedPreferences
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -24,11 +29,46 @@ class LoginActivity : AppCompatActivity() {
             insets
         }
 
+        val db = DBHelper(this)
+
+        sharedPreferences = application.getSharedPreferences("login", Context.MODE_PRIVATE)
+
         /*
-        Salta para a MainActivity, onde será mostrado a lista de contatos
+        Salta para a MainActivity depois de uma serie de validações de login
+        1) Verifica se os campos estão vazios
+        2) Verifica se o usuário existe no banco de dados
+        3) Verifica se o usuário quer ser logado
          */
         binding.buttonLogin.setOnClickListener {
-            startActivity(Intent(this, MainActivity::class.java))
+            val username = binding.editUsername.text.toString()
+            val password = binding.editPassword.text.toString()
+            val logged = binding.checkboxLogged.isChecked
+
+            if(username.isNotEmpty() && password.isNotEmpty()){
+                if(db.login(username, password)){
+                    if(logged){
+                        val editor : SharedPreferences.Editor = sharedPreferences.edit()
+                        editor.putString("username", username)
+                        editor.apply()
+                    }
+                    startActivity(Intent(this, MainActivity::class.java))
+                    finish()
+                }else{
+                    Toast.makeText(
+                        applicationContext,
+                        getString(R.string.login_error),
+                        Toast.LENGTH_SHORT
+                    ).show()
+                }
+                binding.editUsername.setText("")
+                binding.editPassword.setText("")
+            }else {
+                Toast.makeText(
+                    applicationContext,
+                    getString(R.string.please_insert_all_required_fields),
+                    Toast.LENGTH_SHORT
+                ).show()
+            }
         }
 
         /*
